f75d1258b663d46899116f77fbd6bd34bd1c80d5
zhuwenjoyce <ZHUwen12@!>
2021-11-08 09:31:22 +0800

    update migrate logic

--- src/test/java/com/joyce/joyce/reactive/mongo/压测/Dcoupons数据迁移从mongo到mysql.java
+++ src/test/java/com/joyce/joyce/reactive/mongo/压测/Dcoupons数据迁移从mongo到mysql.java
@@ -10,24 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.DateOperators;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.CriteriaDefinition;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
-import org.springframework.data.relational.core.sql.In;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
 
-import java.time.Instant;
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.limit;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
@@ -51,72 +43,72 @@ public class Dcoupons数据迁移从mongo到mysql {
     @Autowired
     private ReactiveMongoTemplate reactiveMongoTemplate;
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    AtomicInteger count = new AtomicInteger(0);
+    AtomicInteger totalMigrateCount = new AtomicInteger(0);
 
     @Test
     public void migrate() {
-        String startDate = "2016-11-09";
-
-        ZonedDateTime migrateStartTime = ZonedDateTime.from(formatter.withZone(ZoneId.systemDefault()).parse(startDate + " 00:00:00"));
+        String paramStartDate = "2016-11-09";
+        ZonedDateTime migrateStartTime = ZonedDateTime.from(formatter.withZone(ZoneId.systemDefault()).parse(paramStartDate + " 00:00:00"));
         log.info("migrateStartTime = " + migrateStartTime); //2011-05-24T14:15:30+08:00[Asia/Shanghai]
 
-        migrateAll(migrateStartTime);
+        Long startMs = System.currentTimeMillis();
+        migrateByCreateAtBetween(migrateStartTime)
+                .doOnSuccess(dcouponsMysqlModel -> {
+                    log.info("migrate data is success");
+                })
+                .doOnError(throwable -> {
+                    log.error("migrate is error", throwable);
+                })
+                .doFinally(t -> {
+                    Long endMs = System.currentTimeMillis();
+                    log.info("migrate is finished in {} ms", endMs.longValue() - startMs.longValue());
+                })
+                .subscribe();
         while (true) {
 
         }
     }
 
-    private void migrateAll(ZonedDateTime migrateStartTime) {
-        ZonedDateTime migrateEndTime = migrateStartTime.plusDays(1000);
+    private Mono<DcouponsMysqlModel> migrateByCreateAtBetween(ZonedDateTime migrateStartTime) {
+        if (migrateStartTime.compareTo(ZonedDateTime.now()) >= 0) {
+            return Mono.just(DcouponsMysqlModel.builder().build());
+        }
 
-        Aggregation aggregation =  newAggregation(
-                match(where("createAt").gte(migrateStartTime).lte(migrateEndTime)),
-//                project("useCode","price","orderTime")
-//                        .and(DateOperators.DateToString.dateOf("orderTime").toString("%m")).as("month"),
-                group("_id").count().as("idint")
-//                ,sort(Sort.by(Sort.Direction.ASC,"_id"))
-        );
+        ZonedDateTime migrateEndTime = migrateStartTime.plusHours(96);
+        AtomicInteger currentMigrateCount = new AtomicInteger(0);
 
-        int ii = reactiveMongoTemplate.aggregate(aggregation, "aggregation", Integer.class).defaultIfEmpty(0).blockLast();
-        log.info("count = {}", ii);
-//                .doOnNext(model -> {
-//                    log.info("model == {}", model);
-//                })
-//                .doOnSubscribe(subscription -> {
-//                    log.info("start...");
-//                })
-//                .doOnError(throwable -> {
-//                    log.error("catch error.", throwable);
-//                })
-//                .subscribe();
-
-//        Query query = query(where("createAt").gte(migrateStartTime).lte(migrateEndTime)).with(Sort.by("createAt").ascending());
-//        reactiveMongoTemplate.find(query, DcouponsMongoModel.class, "dcoupons")
-//                .flatMap(m -> {
-//                    log.info("model = {}, count = {}", m.getCreateAt().format(formatter), count.addAndGet(1));
-//                    DcouponsMysqlModel dcouponsMysqlModel = DcouponsMysqlModel.builder().vid(m.getVid())
-//                            .swid(m.getSwid())
-//                            .couponId(m.getCouponId())
-//                            .status(m.getStatus())
-//                            .mongoId(m.get_id())
-//                            .validateStartTime(m.getValidateStartTime())
-//                            .validateEndTime(m.getValidateEndTime())
-//                            .issuePlatform(m.getIssuePlatform())
-//                            .issueTimestamp(m.getIssueTimestamp())
-//                            .createTime(m.getCreateAt())
-//                            .updateTime(m.getUpdateAt())
-//                            .build();
-//                    return r2dbcEntityTemplate.insert(dcouponsMysqlModel);
-//                })
-//                .doOnNext(dcouponsMysqlModel -> {
-//                    log.info("num {}, migrate id = {}, createAt = {}", count.addAndGet(1), dcouponsMysqlModel.getMongoId(), dcouponsMysqlModel.getCreateTime().format(formatter));
-//                })
-//                .last()
-//                .map(dcouponsMysqlModel -> {
-//
-//                })
-//                .subscribe()
-//        ;
+        return reactiveMongoTemplate.find(
+                query(where("createAt").gte(migrateStartTime).lte(migrateEndTime)).with(Sort.by("createAt").ascending())
+                        , DcouponsMongoModel.class, "dcoupons")
+                .flatMap(m -> {
+                    DcouponsMysqlModel dcouponsMysqlModel = DcouponsMysqlModel.builder().vid(m.getVid())
+                            .swid(m.getSwid())
+                            .couponId(m.getCouponId())
+                            .status(m.getStatus())
+                            .mongoId(m.get_id())
+                            .validateStartTime(m.getValidateStartTime())
+                            .validateEndTime(m.getValidateEndTime())
+                            .issuePlatform(m.getIssuePlatform())
+                            .issueTimestamp(m.getIssueTimestamp())
+                            .createTime(m.getCreateAt())
+                            .updateTime(m.getUpdateAt())
+                            .build();
+                    return r2dbcEntityTemplate.insert(dcouponsMysqlModel);
+                })
+                .doOnNext(dcouponsMysqlModel -> {
+                    currentMigrateCount.addAndGet(1);
+                })
+                .defaultIfEmpty(DcouponsMysqlModel.builder().build())
+                .last()
+                .doOnNext(dcouponsMysqlModel -> {
+                    log.info("current migrate count = {}, total migrate count = {}, dcoupon vid = {}, createAt between {} and {}"
+                            , currentMigrateCount.get()
+                            , totalMigrateCount.addAndGet(currentMigrateCount.get())
+                            , dcouponsMysqlModel.getVid()
+                            , migrateStartTime.format(formatter)
+                            , migrateEndTime.format(formatter));
+                })
+                .flatMap(dcouponsMysqlModel -> migrateByCreateAtBetween(migrateEndTime));
     }
 
 
